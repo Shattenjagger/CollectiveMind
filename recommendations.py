a441e9e18f817f624ea77a76913663a9826163c3
Danila Balabaev <shattenjagger@yandex.ru>
2015-10-20 21:52:37 +0300

    Pearson similarity, top_matches, get_recommendations

--- recommendations.py
+++ recommendations.py
@@ -1,3 +1,6 @@
+# region Data
+from math import sqrt
+
 critics = {
     'Lisa Rose': {
         'Lady in the Water': 2.5,
@@ -51,6 +54,9 @@ critics = {
 }
 
 
+# endregion
+
+
 def sim_distance(prefs, person1, person2):
     si = {}
     for item in prefs[person1]:
@@ -59,9 +65,67 @@ def sim_distance(prefs, person1, person2):
     if len(si) == 0:
         return 0
     sum_of_squares = sum([
-         pow(prefs[person1][item] - prefs[person2][item], 2) for item in prefs[person1] if
-         item in prefs[person2]
-    ])
+                             pow(prefs[person1][item] - prefs[person2][item], 2) for item in prefs[person1] if
+                             item in prefs[person2]
+                             ])
 
     return 1 / (1 + sum_of_squares)
 
+
+def sim_pearson(prefs, p1, p2):
+    si = {}
+    for item in prefs[p1]:
+        if item in prefs[p2]:
+            si[item] = 1
+    n = len(si)
+    if n == 0:
+        return 0
+
+    sum1 = sum([prefs[p1][item] for item in si])
+    sum2 = sum([prefs[p2][item] for item in si])
+
+    sum1Sq = sum([pow(prefs[p1][item], 2) for item in si])
+    sum2Sq = sum([pow(prefs[p2][item], 2) for item in si])
+
+    pSum = sum([prefs[p2][item] * prefs[p1][item] for item in si])
+
+    num = pSum - (sum1 * sum2) / n
+    den = sqrt((sum1Sq - pow(sum1, 2) / n) * (sum2Sq - pow(sum2, 2) / n))
+
+    if den == 0:
+        return 0
+
+    return num / den
+
+
+def top_matches(prefs, person, n=5, similarity=sim_pearson):
+    scores = [
+        (similarity(prefs, person, other), other) for other in prefs if other != person
+        ]
+
+    scores.sort()
+    scores.reverse()
+
+    return scores[0:n]
+
+
+def get_recommendations(prefs, person, similarity=sim_pearson):
+    totals = {}
+    simSums = {}
+
+    for other in prefs:
+        if other == person:
+            continue
+        sim = similarity(prefs, person, other)
+        if sim <= 0:
+            continue
+        for item in prefs[other]:
+            if item not in prefs[person] or prefs[person][item] == 0:
+                totals.setdefault(item, 0)
+                totals[item] += prefs[other][item] * sim
+                simSums.setdefault(item, 0)
+                simSums[item] += sim
+    rankings = [(total / simSums[item], item) for item, total in totals.items()]
+    rankings.sort()
+    rankings.reverse()
+    return rankings
